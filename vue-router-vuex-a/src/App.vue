4e6264d0a35a370cf8d3e7c909205123da0a627b
suximan12 <1542216702@qq.comgit config --global user.email 1542216702@qq.com>
2019-06-09 22:09:04 +0800

    112

---
+++ vue-router-vuex-a/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <!-- <img width="25%" src="./assets/logo.png"> -->
+    <!-- <HelloWorld/> -->
+
+    <!-- <p>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </p> -->
+    <Nav></Nav>    
+    <router-view class="router"></router-view>
+    <!-- <Home/> -->
+  </div>
+</template>
+
+<script>
+import "normalize.css"
+import HelloWorld from "./components/HelloWorld";
+import Home from "./views/Home.vue";
+import Nav from "./views/Nav.vue";
+export default {
+  name: "App",
+  components: {
+    // HelloWorld
+    Home,
+    Nav
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+}
+.router{
+  height: calc(100vh - 51px);
+  }
+</style>
