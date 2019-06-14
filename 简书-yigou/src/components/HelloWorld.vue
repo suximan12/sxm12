5b7ad0299304afe5a3c9b6217010ca3e3887175f
suximan12 <1542216702@qq.comgit config --global user.email 1542216702@qq.com>
2019-06-14 16:43:59 +0800

    简书-已购

---
+++ 简书-yigou/src/components/HelloWorld.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="hello">
+    <ul>
+        <li>
+          <p>
+            <a href="#"><img src="https://i.loli.net/2019/06/06/5cf8b69ac8ff127392.jpg" alt="wuxiaobu.jpg" title="wuxiaobu.jpg" />
+            <span>吴晓布</span></a>
+            <span>2017.12.06</span>
+            <span>21:00</span>
+          </p>
+          <div class="card_c">
+          <h4>湖南|烟雨凤凰古城</h4>
+          <span>
+          1.凤凰古城，边城浪子。如果我是边城，那么你就是浪子。<br> 一次偶然，我和詹姆斯吃着饭，詹姆斯说来了这么久也没<br>有出去，不如周末开车去哪里转转。我...
+          </span>
+          <p><img src="https://i.loli.net/2019/06/06/5cf8b72c2d8f729131.jpg" alt="gucheng.jpg" title="gucheng.jpg" />
+          </p>
+          </div>
+        </li>
+        <li>
+                   <p>
+            <a href="#"><img src="https://i.loli.net/2019/06/06/5cf8b69ac8ff127392.jpg" alt="wuxiaobu.jpg" title="wuxiaobu.jpg" />
+            <span>吴晓布</span></a>
+            <span>2017.12.06</span>
+            <span>21:00</span>
+          </p>
+          <div class="card_c">
+          <h4>湖南|烟雨凤凰古城</h4>
+          <span>
+          1.凤凰古城，边城浪子。如果我是边城，那么你就是浪子。<br> 一次偶然，我和詹姆斯吃着饭，詹姆斯说来了这么久也没<br>有出去，不如周末开车去哪里转转。我...
+          </span>
+          <p><img src="https://i.loli.net/2019/06/06/5cf8b72c2d8f729131.jpg" alt="gucheng.jpg" title="gucheng.jpg" />
+          </p>
+          </div>
+          </li>
+      
+      </ul>
+   
+  </div>
+</template>
+
+<script>
+export default {
+ 
+};
+</script>
+
+
+<style scoped lang="scss">
+.hello{
+  width: 100%;
+  ul{
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    // background-color:aqua;
+    li{
+       list-style: none;
+       border-bottom:1px solid #969696;
+       display: flex;
+       flex-direction: column;
+       text-align: left;
+       padding-bottom: 24px;
+       p{
+        height: auto;
+         a{
+           text-decoration: none;
+           img{
+           width: 30px;
+           border-radius: 15px;
+         }
+         span{
+            color:#333333;
+            margin-left: 10px;
+            font-size: 16px;
+           }
+         }
+         span{
+           font-size: 12px;
+           color: #969696;
+           padding: 0px 10px;
+         }
+       }
+       .card_c{
+         margin-top: -20px;
+          h4{
+          color: #333333;
+          }
+          span{
+          line-height: 30px;
+          color: #A5A5A5;
+          position: absolute;
+          margin-top: -10px;
+          }
+          p{
+            text-align: end;
+          img{
+            width: 20%;
+            margin-right: 30px;
+            margin-top: -45px;
+          }
+        }
+       }
+    }
+  }
+}
+</style>
